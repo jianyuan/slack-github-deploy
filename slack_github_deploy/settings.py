fc9adc214f64a8e2183946ed8d02fdefba54ecad
Jian Yuan Lee <jianyuan@gmail.com>
2018-04-25 03:07:52 +0100

    configure social auth plugin for GitHub and Slack

--- slack_github_deploy/settings.py
+++ slack_github_deploy/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
     'slack_github_deploy',
 ]
 
@@ -64,6 +65,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -77,6 +80,15 @@ WSGI_APPLICATION = 'slack_github_deploy.wsgi.application'
 DATABASES = {
     'default': env.db('DATABASE_URL'),
 }
+
+# Authentication backends
+# https://docs.djangoproject.com/en/2.0/ref/settings/#authentication-backends
+
+AUTHENTICATION_BACKENDS = [
+    'social_core.backends.github.GithubOAuth2',
+    'social_core.backends.slack.SlackOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -117,3 +129,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Social auth
+# http://python-social-auth.readthedocs.io/en/latest/configuration/django.html
+
+SOCIAL_AUTH_POSTGRES_JSONFIELD = True
+
+# GitHub integration
+
+SOCIAL_AUTH_GITHUB_KEY = env('GITHUB_CLIENT_ID')
+SOCIAL_AUTH_GITHUB_SECRET = env('GITHUB_CLIENT_SECRET')
+
+# Slack integration
+
+SOCIAL_AUTH_SLACK_KEY = env('SLACK_CLIENT_ID')
+SOCIAL_AUTH_SLACK_SECRET = env('SLACK_CLIENT_SECRET')
+
+SLACK_VERIFICATION_TOKEN = env('SLACK_VERIFICATION_TOKEN')
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')  # TODO: Remove me
