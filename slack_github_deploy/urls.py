51f3ec31d67117f91c81abe0e2cc85fa5ab73d79
Jian Yuan Lee <jianyuan@gmail.com>
2018-04-29 13:05:49 +0100

    temporary root view

--- slack_github_deploy/urls.py
+++ slack_github_deploy/urls.py
@@ -13,13 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 from slack_github_deploy import views
 
 urlpatterns = [
+    path('', views.temp_root, name='root'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('social_django.urls', namespace='social')),
-    path('integrations/slack/install/', views.install_slack),
+    path('integrations/slack/install/', views.install_slack, name='integrations_slack_install'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
--- slack_github_deploy/views.py
+++ slack_github_deploy/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
+
+
+def temp_root(request):
+    return render(request, 'temp_root.html')
 
 
 def install_slack(request):
